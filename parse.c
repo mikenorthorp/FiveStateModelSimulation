6c599c4d59007497436c9d48cb352517addc30e0
Michael Northorp <mike.northorp@gmail.com>
2013-05-24 07:51:59 -0700

    Added SIGALRM handler that reads in from config timer and constantly interrupts

--- parse.c
+++ parse.c
@@ -8,101 +8,157 @@
 #define BUF_MAX  (81)
 #define NAME_MAX (21)
 
+// Global variables for timer and queues
+int timer = 0;
+List_t processes;
+List_t running;
+List_t blocked;
+
+/* This function overides the default SIGALRM and handles all of the state
+   transitions for the different processes */
+void
+stateTransitions( int the_signal )
+{
+    printf ("One time unit has passed\n");
+
+    alarm( timer );
+}
+
+
+/* Set up SIGALRM with custom timer from config.txt */
+int
+setUpAlarm(int timer)
+{
+    int return_code = 0;
+
+    if (signal( SIGALRM, stateTransitions ) == SIG_ERR)
+    {
+        printf ("Unable to install handler\n");
+        return_code = 1;
+    }
+    else
+    {
+        /* Start the timer and then wait! */
+        alarm( timer );
+    }
+
+    return return_code;
+}
+
 /* This reads in the timer for the unit of time from
    the config.txt file */
 int
-readConfigTimer() {
-  FILE *inFile;
-  int unitOfTime = 0;
+readConfigTimer()
+{
+    FILE *inFile;
+    int unitOfTime = 0;
 
-  // Open file
-  inFile = fopen("config.txt", "r");
+    // Open file
+    inFile = fopen("config.txt", "r");
 
-   // Check that file exits
-  if(inFile == NULL) {
-    printf("Error: File not found\n");
+    // Check that file exits
+    if (inFile == NULL)
+    {
+        printf("Error: File not found\n");
+        return unitOfTime;
+    }
+
+    // Scan in the unit of time in the format timer=[num]
+    fscanf(inFile, "timer=%d", &unitOfTime);
+    printf("The timer is %d", unitOfTime);
+
     return unitOfTime;
-  }
-
-  // Scan in the unit of time in the format timer=[num]
-  fscanf(inFile, "timer=%d", &unitOfTime);
-  printf("The timer is %d", unitOfTime);
-
-  return unitOfTime;
 }
 
 /* Define the struct for the PCB
    Takes in name, lifetime and running time */
-typedef struct {
-  char name[NAME_MAX];
-  int  lifetime;
-  int  runningTime;
+typedef struct
+{
+    char name[NAME_MAX];
+    int  lifetime;
+    int  runningTime;
 } pcb_t;
 
 // Main function
 int
 main( int argc, char **argv )
 {
-  // Declare initial variables
-  int    error_code = 0;  /* In unix, a program returns 0 if all is ok. */
-  List_t processes;
-  char   inputbuffer[BUF_MAX];
-  char   name[BUF_MAX];
-  int    lifetime;
-  int    runningTime;
-  int    timer;
-  pcb_t  *pcb;
+    // Declare initial variables
+    int    error_code = 0;  /* In unix, a program returns 0 if all is ok. */
+    char   inputbuffer[BUF_MAX];
+    char   name[BUF_MAX];
+    int    lifetime;
+    int    runningTime;
+    pcb_t  *pcb;
 
-  /* Initialize my data structures. */
-  if (List_init( &processes )) {
+    /* Initialize my data structures. */
+    if (List_init( &processes ) && List_init( &running ) && List_init( &blocked ))
+    {
 
-    /* This creates an infinite loop from which you will need to do a
-       ^C to stop the program.  This may not be the most elegant solution
-       for you. */
-    while (1) {
-      if (fgets( inputbuffer, BUF_MAX-1, stdin )) {
-      	/* Put the parameters into a PCB and store it in the list of processes. */
-      	if (sscanf( inputbuffer, "%s %d %d", name, &lifetime, &runningTime) == 3) {
-        /* We have all the input that is required. */
+        timer = readConfigTimer();
+        //Set up alarm code
+        setUpAlarm(timer);
 
-          // Allocate memory for PCB
-      	  pcb = (pcb_t *) malloc( sizeof( pcb_t ) );
-      	  if (pcb != NULL) {
-      	    strncpy( pcb->name, name, NAME_MAX-1 );
-      	    pcb->name[NAME_MAX] = '\0'; /* Make sure that it is null-terminated. */
-      	    pcb->lifetime = lifetime;
-            pcb->runningTime = runningTime;
+        /* This creates an infinite loop from which you will need to do a
+           ^C to stop the program.  This may not be the most elegant solution
+           for you. */
+        while (1)
+        {
+            if (fgets( inputbuffer, BUF_MAX - 1, stdin ))
+            {
+                /* Put the parameters into a PCB and store it in the list of processes. */
+                if (sscanf( inputbuffer, "%s %d %d", name, &lifetime, &runningTime) == 3)
+                {
+                    /* We have all the input that is required. */
 
-            //Show that process has been read and stored
-      	    printf ("Read and stored process %s with lifetime %d and running time of %d\n", pcb->name, pcb->lifetime, pcb->runningTime);
+                    // Allocate memory for PCB
+                    pcb = (pcb_t *) malloc( sizeof( pcb_t ) );
+                    if (pcb != NULL)
+                    {
+                        strncpy( pcb->name, name, NAME_MAX - 1 );
+                        pcb->name[NAME_MAX] = '\0'; /* Make sure that it is null-terminated. */
+                        pcb->lifetime = lifetime;
+                        pcb->runningTime = runningTime;
 
-            // Could not insert process into list
-      	    if (List_add_tail( &processes, (void *)pcb ) == 0) {
-      	      printf ("Error in inserting the process into the list.\n");
-      	    }
+                        //Show that process has been read and stored
+                        printf ("Read and stored process %s with lifetime %d and running time of %d\n", pcb->name, pcb->lifetime, pcb->runningTime);
 
-          // Error, not enough memory to make PCB
-      	  } else {
-      	    printf( "Unable to allocate enough memory.\n");
-      	  }
+                        // Could not insert process into list
+                        if (List_add_tail( &processes, (void *)pcb ) == 0)
+                        {
+                            printf ("Error in inserting the process into the list.\n");
+                        }
 
-        // Incorrect number of parameters read in
-      	} else {
-      	  printf ("Incorrect number of parameters read.\n");
-      	}
+                        // Error, not enough memory to make PCB
+                    }
+                    else
+                    {
+                        printf( "Unable to allocate enough memory.\n");
+                    }
 
-      // No paramters read in
-      } else {
-	      printf ("Nothing read in.\n");
-      }
+                    // Incorrect number of parameters read in
+                }
+                else
+                {
+                    printf ("Incorrect number of parameters read.\n");
+                }
+
+                // No paramters read in
+            }
+            else
+            {
+                printf ("Nothing read in.\n");
+            }
+        }
+
+    }
+    else
+    {
+        printf ("Unable to initialize the list of processes.\n");
+        error_code = 1;
     }
 
-  } else {
-    printf ("Unable to initialize the list of processes.\n");
-    error_code = 1;
-  }
-
-  // End of program
-  return error_code;
+    // End of program
+    return error_code;
 }
 
